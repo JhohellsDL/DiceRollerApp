e5163f8cc5854497a3f6276bae639a6ed5fe00d4
Jhohells Erick Dianderas Lopez <45276003+JhohellsDL@users.noreply.github.com>
2022-10-10 22:50:10 -0500

    Unit Test

--- app/src/test/java/com/example/dicerollerapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/dicerollerapp/ExampleUnitTest.kt
@@ -11,7 +11,9 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number() {
+        val dice = MainActivity.Dice(4)
+        val rollResult = dice.roll()
+        assertTrue("estas en el rango",rollResult in 1..6)
     }
 }
